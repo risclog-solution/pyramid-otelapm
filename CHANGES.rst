1fb3c9e3ca77b2e929d1f53872bd89478b321f22
Sebastian Wehrmann <sebastian@risclog.com>
2026-09-18 07:41:11 +0200

    Back to development: 1.0.1

--- CHANGES.rst
+++ CHANGES.rst
@@ -3,6 +3,12 @@ Change log for pyramid-otelapm
 ==============================
 
 
+1.0.1 (unreleased)
+==================
+
+- Nothing changed yet.
+
+
 1.0.0 (2026-09-18)
 ==================
 
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pyramid-otelapm',
-    version='1.0.0',
+    version='1.0.1.dev0',
     python_requires='>=3.11',
     install_requires=[
         'opentelemetry-api',
